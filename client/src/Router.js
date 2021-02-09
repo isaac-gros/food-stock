5d470d4137daf47a0fa19a8c9bca90a3c69cf493
LucasLaurens <lucaslaurens.74@gmail.com>
2021-02-09 12:31:15 +0100

    [Init] add some package for navigation

--- client/src/Router.js
+++ client/src/Router.js
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 // Screens import
 import HomeScreen from "./screens/HomeScreen";
-import Home2Screen from "./screens/Home2Screen";
 
 const Stack = createStackNavigator();
 export default function Router() {
@@ -12,7 +11,6 @@ export default function Router() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
-        <Stack.Screen name="Home2" options={{ headerShown: false }} component={Home2Screen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
