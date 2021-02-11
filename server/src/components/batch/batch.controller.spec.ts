7153e97042bbda51a6c74290b29e4e9c29975698
MaximePocard <pocard.maxime@gmail.com>
2021-02-11 12:30:58 +0100

    [TEST] added unit test for batch controller

--- server/src/components/batch/batch.controller.spec.ts
+++ server/src/components/batch/batch.controller.spec.ts
@@ -7,17 +7,30 @@ import { BatchService } from './batch.service';
 describe('BatchController', () => {
   let controller: BatchController;
 
-  const mockRepository = {
-    find: jest.fn()
+  const mockService = {
+    create: jest.fn(),
+    modify: jest.fn(),
+    delete: jest.fn()
   }
+
+  const mockBatch =   {
+    id: 1,
+    category: {
+      id: 1,
+      name: 'Fruit'
+    },
+    batch: {
+      id: 1
+    },
+    quantity: 3
+  } as unknown as Batch
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        BatchService,
         {
-          provide: getRepositoryToken(Batch),
-          useValue: mockRepository
+          provide: BatchService,
+          useValue: mockService
         }
       ],
       controllers: [BatchController],
@@ -29,4 +42,46 @@ describe('BatchController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('create', () => {
+    it('should create a batch', async () => {
+      jest.spyOn(mockService, 'create').mockResolvedValue(mockBatch)
+
+      expect(mockService.create).not.toHaveBeenCalled();
+
+      const batch = await controller.create(mockBatch)
+
+      expect(mockService.create).toHaveBeenCalled();
+      expect(mockService.create).toHaveBeenCalledWith(mockBatch);
+      expect(batch).toEqual(mockBatch)
+    });
+  })
+
+  describe('modify', () => {
+    it('should modify a batch', async () => {
+      jest.spyOn(mockService, 'modify').mockResolvedValue(mockBatch)
+
+      expect(mockService.modify).not.toHaveBeenCalled();
+
+      const batch = await controller.modify(1, mockBatch)
+
+      expect(mockService.modify).toHaveBeenCalled();
+      expect(mockService.modify).toHaveBeenCalledWith(1, mockBatch);
+      expect(batch).toEqual(mockBatch)
+    });
+  })
+
+  describe('delete', () => {
+    it('should delete a batch', async () => {
+      jest.spyOn(mockService, 'delete').mockResolvedValue(mockBatch)
+
+      expect(mockService.delete).not.toHaveBeenCalled();
+
+      const batch = await controller.delete(2)
+
+      expect(mockService.delete).toHaveBeenCalled();
+      expect(mockService.delete).toHaveBeenCalledWith(2);
+      expect(batch).toEqual(mockBatch)
+    });
+  })
 });
