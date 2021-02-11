5937c116e97da8d875c7e729a742bc92bb9b2eed
MaximePocard <pocard.maxime@gmail.com>
2021-02-11 07:20:39 +0100

    [FIX] resolve dependencies injection error

--- server/src/components/category/category.service.spec.ts
+++ server/src/components/category/category.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Category } from './category.entity';
 import { CategoryService } from './category.service';
 
 describe('CategoryService', () => {
   let service: CategoryService;
 
+  const mockRepository = {
+    find: jest.fn()
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [
+        CategoryService,
+        {
+          provide: getRepositoryToken(Category),
+          useValue: mockRepository
+        }
+      ],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
